f2f74ca38ec80f39c2befaa1295b16d5735f288e
wyoung163 <20201014@sungshin.ac.kr>
2023-07-19 23:39:02 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express"); // express 모듈 셋팅
+const ejs = require("ejs"); // 페이지 로딩을 위해 필수
+const app = express();
+
+// view 엔진을 ejs를 쓰겠다는 설정
+app.set("view engine", "ejs");
+
+// 페이지 로딩 함수
+app.get("/", function(req, res){
+    //console.log(res);
+    res.render("test", {}); // views 폴더 밑에 있는 파일을 참조함
+});
+
+// 카카오페이 결제창 호출
+app.get("/pay", function(req, res){
+    res.render("payment", {}); // views 폴더 밑에 있는 파일을 참조함
+});
+
+// 서버 띄울때 포트 정보 셋팅 및 처음 실행 시 필요한 기능 수행 가능
+app.listen(3000, function(){
+    console.log("server running");
+});
